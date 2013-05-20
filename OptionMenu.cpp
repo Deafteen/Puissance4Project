8a6203c1b2c99aa7be31fbcca0a3ae4503ef53ba
hugo taboada <hugo@mint>
2013-05-20 04:31:25 +0200

    implementation de option menu

--- OptionMenu.cpp
+++ OptionMenu.cpp
@@ -2,31 +2,24 @@
 #include "OptionMenu.h"
 
 
-OptionMenu::OptionMenu(Puissance4* puissance4P)
+OptionMenu::OptionMenu(Puissance4* puissance4P) : Menu(puissance4P)
 {
-    puissance4 = puissance4P;
-    curseur = NULL;
     img = SDL_LoadBMP("Images/grid.bmp");//on a pas d image pour option menu
-    image = NULL;
     closeApplicationObject = NULL;
 }
 
 
-OptionMenu::~OptionMenu(void)
-{
-    SDL_FreeSurface(img);
+OptionMenu::~OptionMenu(void){
+
 }
 
 void OptionMenu::start() {
-
     curseur = new Curseur(puissance4->getEngineManager());
     image = new Image(puissance4->getEngineManager(), img);
-
     closeApplicationObject = new CloseApplicationObject(puissance4->getEngineManager());
 }
 
 void OptionMenu::stop() {
-
     delete curseur;
     delete image;
     delete closeApplicationObject;
--- OptionMenu.h
+++ OptionMenu.h
@@ -1,13 +1,10 @@
 #pragma once
-#include "State.h"
-#include "Curseur.h"
+#include "Menu.h"
 #include "CloseApplicationObject.h"
-#include "SDL/SDL.h"
-#include "Image.h"
 
 class Puissance4;
 
-class OptionMenu: public State
+class OptionMenu: public Menu
 {
 public:
     OptionMenu(Puissance4*);
@@ -15,12 +12,7 @@ public:
 
     void start();
     void stop();
-
 private:
-    Puissance4* puissance4;
-    Curseur* curseur;
-    Image * image;
-    SDL_Surface* img;
     CloseApplicationObject *closeApplicationObject;
 };
 
